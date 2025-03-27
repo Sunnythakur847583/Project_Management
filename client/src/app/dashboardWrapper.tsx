5b64e322ca2791be4a64accd413f5f7dc6e3b3f8
sunny <sunnykumarthakur29907@gmail.com>
2025-03-27 16:55:59 +0530

    Sidebar + Navbar + Redux added

---
+++ client/src/app/dashboardWrapper.tsx
@@ -0,0 +1,41 @@
+"use client";
+import React, { useEffect } from "react";
+import Navbar from "./(components)/Navbar";
+import Sidebar from "./(components)/sidebar";
+import StoreProvider, { useAppSelector } from "./redux";
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed
+  );
+
+  const IsDarkMode = useAppSelector((state) => state.global.isDarkMode);
+  useEffect(() => {
+    if (IsDarkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  });
+  return (
+    <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
+      {/*sidebar */}
+      <Sidebar />
+      <main
+        className={`bg-gray-20 dark:bg-dark-bg flex w-full flex-col ${isSidebarCollapsed ? "" : " md:pl-64"}`}
+      >
+        {/*Navbar */}
+        <Navbar />
+        {children}
+      </main>
+    </div>
+  );
+};
+
+const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <StoreProvider>
+      <DashboardLayout>{children}</DashboardLayout>
+    </StoreProvider>
+  );
+};
+export default DashboardWrapper;

bcc0dafee3e6f8c440139df7986be0c503c2c561
sunny <sunnykumarthakur29907@gmail.com>
2025-04-16 16:41:09 +0530

    redirect images to s3

--- client/next.config.js
+++ client/next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    remotePstterns: [
+      {
+        protcol: "https",
+        hostname: "mypms3-images.s3.us-east-1.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
